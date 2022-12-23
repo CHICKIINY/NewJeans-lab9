51da6875feafa695a8d5a3e0fb9f7ba7ce9eb3e7
CHICKIINY <bb.bright36389@gmail.com>
2022-12-23 16:49:05 +0700

    done

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,54 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int A;
+    cout << "Enter your age: ";
+    cin >> A;
+    if(A <= 25)
+    {
+        int h;
+        cout << "Enter your height: ";
+        cin >> h;
+        if(h < 100)
+        {
+              cout << "Your character = Chopper";  
+        }else{
+            if(h < 180)
+            {
+              cout << "Your character = Usopp";    
+            }else{
+                int B;
+            cout << "Enter your bounty: ";
+            cin >> B;
+            if(B > 1100000000)
+            {
+                cout << "Your character = Zoro";    
+            }else{
+                cout << "Your character = Sanji";   
+            }    
+            }
+        }
+    }else{
+        if(A <= 60)
+        {
+            int B;
+            cout << "Enter your bounty: ";
+            cin >> B;
+            if(B > 500000000)
+            {
+                cout << "Your character = Jinbe";
+            }else{
+                cout << "Your character = Franky";
+            }
+        }else{
+            cout << "Your character = Brook";
+        }
+    }
+    return 0;
+}
